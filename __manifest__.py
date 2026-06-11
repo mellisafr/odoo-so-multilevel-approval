8ce71511b32ddbbd1e7b171540e9aeea4d5564da
Mellisa Fitriana <mellisa@cnt.id>
2026-06-11 18:36:24 +0700

    Update author and website information in __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,8 @@
     'version': '17.0.1.0.0',
     'summary': 'Approval workflow for sales orders',
     'category': 'Sales',
-    'author': 'CNT',
-    'website': '',
+    'author': 'Mellisa Fitriana',
+    'website': 'https://www.linkedin.com/in/mellisafr/',
     'license': 'LGPL-3',
     'depends': ['sale_management', 'sale_stock'],
     'data': [
